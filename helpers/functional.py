e128008a0fef0e078649e3f47fd4c0c206bf1a86
ophite <kobernik.yura@gmail.com>
2014-10-15 14:22:39 +0300

    delete functional.py

--- helpers/functional.py
+++
@@ -1,5 +0,0 @@
-class curry:
-    def __init__(self, func, *args, **kwargs):
-        self._func, self._params, self._pos = func, args, kwargs.get('pos', 0)
-    def __call__(self, *args):
-        return self._func(*(args[:self._pos] + self._params + args[self._pos:]))
